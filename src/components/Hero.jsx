9fe36d3c4a75ac5e80bd59637487686bf8485114
amirah <mirez.data@gmail.com>
2024-02-26 22:54:48 +0800

    make changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,12 +5,9 @@ import heroImage from "../assets/hero.jpg";
 function Hero(){
 
     return(
-        <div className="max-w-full bg-cover bg-center text-white"
-        style={{ backgroundImage: `url(${heroImage})` }}
-            
-        id="home">
-            <div className="max-w-[800px] mt-[-96px] w-full h-[500px] text-center mx-auto flex flex-col justify-center"
-            >
+        <div className="max-w-full  bg-center text-white mt-[5%]" id="home">
+            <div className="bg-cover max-w-full h-[700px] mt-[-96px] w-full text-center flex flex-col justify-center"
+             style={{ backgroundImage: `url(${heroImage})` }} >
                 <h1 className="text-4xl font-bold md:text-7xl sm:text-5xl md:py-6">Capturing Timeless Memories of Your Special Day</h1>
                 <p className="text-xl p-2 mt-[30px]">Preserving cherished moments with artful photography, we capture the essence of your love story, ensuring timeless memories of your special day.</p>
                 <button className="w-[150px] bg-[#F5EEE6] text-l uppercase text-[#E6A4B4] rounded-md my-4 py-2 mx-auto hover:bg-[#F3D7CA] hover:text-white">Book with us</button>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,52 @@
 import React from "react";
+import { RiMenu4Fill, RiCloseFill } from "react-icons/ri";
 
 function Navbar(){
+const [nav, setNav] = React.useState(false)
+
+const handleNav=() => {
+    setNav(!nav)
+}
+
     return(
-        <div className="flex justify-between items-center max-w-[1240px] h-24 bg-[#FFF8E3] mx-auto px-3">
-            <h2 className="w-full font-bold text-2xl text-[#E6A4B4]">EleganceStudio</h2>
-            <ul>
-                <li>
+        <div className="flex justify-between items-center max-w-full h-24 sm:h-32 bg-fuchsia-200/50  mx-auto px-5 text-white">
+            <h2 className="w-full font-bold text-2xl p-2 sm:mx-auto">EleganceStudio</h2>
+            <ul className="hidden md:flex">
+                <li className="p-2 text-xl hover:border-b"> 
                     <a href="#home">Home</a>
                 </li>
-                <li>
+                <li className="p-2 text-xl hover:border-b">
                     <a href="#about">About</a>
                 </li>
-                <li>
+                <li className="p-2 text-xl hover:border-b">
                     <a href="#services">Services</a>
                 </li>
-                <li>
+                <li className="p-2 text-xl hover:border-b">
                     <a href="#contact">Contact</a>
                 </li>
             </ul>
+
+            <div onClick={handleNav} className="block md:hidden">
+                { nav? <RiCloseFill size={30} /> : <RiMenu4Fill size={30} /> }
+            </div>
+
+            <div className= {nav? "fixed top-0 h-[30%] w-full border-r bg-[#E6A4B4]" : "fixed top-[-100%] "}>
+            <ul className="p-2 uppercase flex">
+                <li className="p-2 text-xl hover:border-b"> 
+                    <a href="#home">Home</a>
+                </li>
+                <li className="p-2 text-xl hover:border-b">
+                    <a href="#about">About</a>
+                </li>
+                <li className="p-2 text-xl hover:border-b">
+                    <a href="#services">Services</a>
+                </li>
+                <li className="p-2 text-xl hover:border-b">
+                    <a href="#contact">Contact</a>
+                </li>
+            </ul>
+
+            </div>
 
         </div>
     )
